e16df258399dc0d844dd8dd0bb428d1c316908ea
Adam Liberadzki <adam.liberadzki@gmail.com>
2016-09-24 12:15:15 +0200

    obsluga zlej kolejnosci wywolania metod w builderze (exception)

---
+++ src/main/java/pl/com/sages/hr/exceptions/WrongOrganizationStructureException.java
@@ -0,0 +1,10 @@
+package pl.com.sages.hr.exceptions;
+
+/**
+ * Created by aliberadzki on 24.09.16.
+ */
+public class WrongOrganizationStructureException extends Throwable {
+    public WrongOrganizationStructureException(String message) {
+        super(message);
+    }
+}
